794a690012cef200fed0f619bb35f90f1fba0f99
Jun Loke <rebecca.j.loke@gmail.com>
2018-08-27 20:33:47 -0400

    Removed debugging from Collision module

--- my_modules/my_collision.js
+++ my_modules/my_collision.js
@@ -4,24 +4,15 @@ const TILE_SIZE = 50;
   */
   function collide(player, obstacle) {
     var coordinates= {x: player.x, y: player.y};
-console.log(player);
-console.log(obstacle);
+
     if ((Math.abs(player.x-obstacle.x)<TILE_SIZE)&& (Math.abs(player.y-obstacle.y)<TILE_SIZE)){
      // taller than wide, shift horizontalLy
       if (Math.abs(player.x-obstacle.x)>Math.abs(player.y-obstacle.y)){
-
-        console.log('shift horizontalLy by '+(player.x-obstacle.x));
         coordinates.x=(player.x+(player.x-obstacle.x));
       }
       else { // if wider than tall, shift vertically
-        console.log('shift vertically by '+(player.y-obstacle.y));
         coordinates.y=(player.y+(player.y-obstacle.y));
       }
-    }else {
-      console.log("Sucks to suck");
-    }
-
-    console.log("coordinates = ["+ coordinates.x+", "+ coordinates.y+"]");
     return coordinates;
   }//end of function
 module.exports = collide;

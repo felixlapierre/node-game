6bf645105edd38b5b1ec60739b4ea61911d1b258
Jun Loke <rebecca.j.loke@gmail.com>
2018-08-29 21:20:35 -0400

    Trying new collision detection with debugging logs

--- my_modules/my_collision.js
+++ my_modules/my_collision.js
@@ -1,24 +1,37 @@
 const TILE_SIZE = 50;
+const HALF_TILE = 25;
 /*collide
 returns an object containing the x-y coordinates of player AFTER collision resolution
 */
 function wallCollide(playerX, playerY, obstacleX, obstacleY) {
+  console.log('player x: '+ playerX);
+  console.log('player y: '+ playerY);
+  console.log('obstacle x: '+ obstacleX);
+  console.log('obstacle y: '+ obstacleY);
+  console.log();
   var coordinates= {x: playerX, y: playerY};
-
-  if ((Math.abs(playerX-obstacleX)<TILE_SIZE)&& (Math.abs(playerY-obstacleY)<TILE_SIZE)){
-    // taller than wide, shift horizontalLy
-    if (Math.abs(playerX-obstacleX)>Math.abs(playerY-obstacleY)){
-      console.log("horiz shift");
-      console.log(playerX-obstacleX);
-      coordinates.x=(playerX+(playerX-obstacleX));
-    }
-    else { // if wider than tall, shift vertically
-      console.log("vert shift");
-      console.log(playerY-obstacleY);
-      coordinates.y=(playerY+(playerY-obstacleY));
-    }
+    //if left bound of player is beyond right bound of obstacle
+  if ((playerX-HALF_TILE)>(obstacleX+HALF_TILE)){
+  coordinates.x = obstacleX+TILE_SIZE;
   }
-  return coordinates;
+  if ((playerX+HALF_TILE)>(obstacleX-HALF_TILE)){
+  coordinates.x = obstacleX-TILE_SIZE;
+  }
+  /*if ((Math.abs(playerX-obstacleX)<TILE_SIZE)|| (Math.abs(playerY-obstacleY)<TILE_SIZE)){
+  // wider
+  if (Math.abs(playerX-obstacleX)>Math.abs(playerY-obstacleY)){
+  console.log("horiz shift");
+  console.log(playerX-obstacleX);
+  coordinates.x=(playerX+(playerX-obstacleX));
+}
+else if(Math.abs(playerX-obstacleX)<Math.abs(playerY-obstacleY)){ // if wider than tall, shift vertically
+console.log("vert shift");
+console.log(playerY-obstacleY);
+coordinates.y=(playerY+(playerY-obstacleY));
+}
+}*/
+
+return coordinates;
 }//end of function
 
 
@@ -38,41 +51,41 @@ function wallCheck(wallMap, playerX, playerY){
 
 
   var playerTile = {
-    x: parseInt( (playerX-25)/TILE_SIZE),
-    y: parseInt( (playerY-25)/TILE_SIZE)
+    x: parseInt( (playerX-HALF_TILE)/TILE_SIZE),
+    y: parseInt( (playerY-HALF_TILE)/TILE_SIZE)
   };
 
   //check boxes for walls
   for (var i=0; i<1; i++){
     for (var j=0; j<1; j++){
       if (wallMap[playerTile.x+i][playerTile.y+j]==1){
-        return(  wallCollide( playerX, playerY, ( (playerTile.x*TILE_SIZE) +25), ( (playerTile.y*TILE_SIZE) +25) )  );
+        return(  wallCollide( playerX, playerY, ( ((playerTile.x+i)*TILE_SIZE)), ( ((playerTile.y+j)*TILE_SIZE)) )  );
       }
       return coordinates;
     }
   }
 }
-    module.exports.wallCheck = wallCheck;
+module.exports.wallCheck = wallCheck;
 
-    /*
-    boundsCheck
-    checks if player is out of bounds. If out of bounds, player is pushed to closest
-    */
-    function boundsCheck(playerX, playerY, bounds){
-      var coordinates= {x: playerX, y: playerY};
-      // check horizontal
-      if (playerX<25){
-        coordinates.x=25;
-      }else if (playerX>(bounds.x-25)){
-        coordinates.x=(bounds.x-25);
-      }
-      // check vertical
-      if(playerY<25){
-        coordinates.y = 25;
-      }
-      else if(playerY>(bounds.y-25)){
-        coordinates.y = (bounds.y-25);
-      }
-      return coordinates;
-    }
-    module.exports.boundsCheck = boundsCheck;
+/*
+boundsCheck
+checks if player is out of bounds. If out of bounds, player is pushed to closest
+*/
+function boundsCheck(playerX, playerY, bounds){
+  var coordinates= {x: playerX, y: playerY};
+  // check horizontal
+  if (playerX<HALF_TILE){
+    coordinates.x=HALF_TILE;
+  }else if (playerX>(bounds.x-HALF_TILE)){
+    coordinates.x=(bounds.x-HALF_TILE);
+  }
+  // check vertical
+  if(playerY<HALF_TILE){
+    coordinates.y = HALF_TILE;
+  }
+  else if(playerY>(bounds.y-HALF_TILE)){
+    coordinates.y = (bounds.y-HALF_TILE);
+  }
+  return coordinates;
+}
+module.exports.boundsCheck = boundsCheck;

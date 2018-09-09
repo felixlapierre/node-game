e9cc7b1c31161518fbda40f0428794db8c4edf2f
Felix <felix.lapierre1@gmail.com>
2018-09-08 22:34:19 -0400

    Level editor now supports selecting specific textures from the spritesheet and those textures can now be added to the texture map

--- Level Editor/script.js
+++ Level Editor/script.js
@@ -3,11 +3,11 @@
 //
 
 var textureMap = {
-  spritesheet: undefined,
+  spritesheet: "sprites/spritesheet1.png",
   tiles: []
 };
 
-var wallMap = {
+var blockMap = {
   bounds: {
     x: undefined,
     y: undefined
@@ -23,16 +23,17 @@ var topleft = {
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var spritesheet = new Image();
+spritesheet.src = "sprites/spritesheet1.png";
 var origin = new Image();
 origin.src = "sprites/origin.png";
 var hover = new Image();
 hover.src = "sprites/hover.png";
 
 var movement = {
-	up: false,
-	down: false,
-	left: false,
-	right: false,
+  up: false,
+  down: false,
+  left: false,
+  right: false,
 }
 
 //Mouse position relative to canvas
@@ -43,7 +44,18 @@ var mouse = {
 //Mouse position relative to map
 var mouseMapPosition = {
   x:0,
-  y:0
+  y:0,
+  recalculate : function() {
+    this.x = mouse.x + topleft.x;
+    this.y = mouse.y + topleft.y;
+  }
+}
+
+var settings = {
+  gridlock: false,
+  adjustingTiling: false,
+  textureTilingX:0,
+  textureTilingY:0
 }
 
 //
@@ -52,51 +64,87 @@ var mouseMapPosition = {
 //
 
 document.addEventListener('keydown', function(event) {
-	switch(event.keyCode) {
-		case 65: //A
-		movement.left = true;
-		break;
-		case 87: //W
-		movement.up = true;
-		break;
-		case 68: //D
-		movement.right = true;
-		break;
-		case 83: //S
-		movement.down = true;
-		break;
-	}
+  //Adjust how much tiling is done
+  if(settings.adjustingTiling == true) {
+    switch(event.keyCode) {
+      case 65: //A
+      settings.textureTilingX -= (settings.textureTilingX == 0) ? 0 : 1;
+      break;
+      case 87: //W
+      settings.textureTilingY -= (settings.textureTilingY == 0) ? 0 : 1;
+      break;
+      case 68: //D
+      settings.textureTilingX += 1;
+      break;
+      case 83: //S
+      settings.textureTilingY += 1;
+      break;    }
+  } else {
+    switch(event.keyCode) {
+      case 65: //A
+      movement.left = true;
+      break;
+      case 87: //W
+      movement.up = true;
+      break;
+      case 68: //D
+      movement.right = true;
+      break;
+      case 83: //S
+      movement.down = true;
+      break;
+    }
+  }
+  //Other keys
+  switch(event.keyCode) {
+    case 69: //E
+    settings.gridlock = !settings.gridlock;
+    movement = {
+      up: false,
+      down: false,
+      left: false,
+      right: false,
+    }
+    break;
+
+    case 16: //Shift
+    settings.adjustingTiling = true;
+    break;
+  }
 });
 
 document.addEventListener('keyup', function(event) {
-	switch(event.keyCode) {
-		case 65: //A
-		movement.left = false;
-		break;
-		case 87: //W
-		movement.up = false;
-		break;
-		case 68: //D
-		movement.right = false;
-		break;
-		case 83: //S
-		movement.down = false;
-		break;
-	}
+  switch(event.keyCode) {
+    case 65: //A
+    movement.left = false;
+    break;
+    case 87: //W
+    movement.up = false;
+    break;
+    case 68: //D
+    movement.right = false;
+    break;
+    case 83: //S
+    movement.down = false;
+    break;
+
+    case 16: //Shift
+    settings.adjustingTiling = false;
+    break;
+  }
 });
 
 document.addEventListener('mousemove', function(event) {
-	mouse.x = event.clientX - canvas.getBoundingClientRect().left - 5; //5 is the width of the border
-	mouse.y = event.clientY - canvas.getBoundingClientRect().top - 5;
-  mouseMapPosition.x = mouse.x + topleft.x;
-  mouseMapPosition.y = mouse.y + topleft.y;
+  mouse.x = event.clientX - canvas.getBoundingClientRect().left - 5; //5 is the width of the border
+  mouse.y = event.clientY - canvas.getBoundingClientRect().top - 5;
+  mouseMapPosition.recalculate();
 }, false);
 
 canvas.addEventListener('click', function(event) {
   handleCanvasClick(mouseMapPosition.x, mouseMapPosition.y);
 }, false)
 
-function Tile(sourceX, sourceY, top, left, bottom, right, width, height, rotation) {
+function Tile(sourceX, sourceY, width, height, top, left, bottom, right, rotation) {
   this.sourceX = sourceX,
   this.sourceY = sourceY,
   this.left = left,
@@ -121,16 +169,40 @@ function drawCanvas() {
 function drawTiles() {
   var obj;
   for(var i = 0; i < textureMap.tiles.length; i++) {
-    obj = textureMap.tiles[i];
-    context.drawImage(spritesheet, obj.sourceX, obj.sourceY, width, height,
-    x - topleft.x, y - topleft.y, width, height);
+    for(var j = 0; j < textureMap.tiles[i].length; j++) {
+      obj = textureMap.tiles[i][j];
+      for(var x = obj.left; x <= obj.right; x += obj.width) {
+        for(var y = obj.top; y <= obj.bottom; y += obj.height) {
+          context.drawImage(spritesheet, obj.sourceX, obj.sourceY, obj.width, obj.height,
+            x - topleft.x, y - topleft.y, obj.width, obj.height);
+        }
+      }
+    }
   }
 }
 
 function drawEditorElements() {
   context.drawImage(origin, 0, 0, 100, 100, -50 - topleft.x, -50 - topleft.y, 100, 100);
-  var hoverTextureXOffset = (mouseMapPosition.x < 0 || mouseMapPosition.y < 0) ? 50 : 0;
-  context.drawImage(hover, hoverTextureXOffset, 0, 50, 50, mouse.x, mouse.y, 50, 50);
+
+  //Is the hovered location out of bounds?
+  if (!(mouseMapPosition.x < 0 || mouseMapPosition.y < 0)) {
+    //Apply gridlock to hovered location indicater
+    var hoverX, hoverY;
+    if(settings.gridlock == true) {
+      hoverX = parseInt(mouseMapPosition.x / 50.0) * 50 - topleft.x;
+      hoverY = parseInt(mouseMapPosition.y / 50.0) * 50 - topleft.y;
+    } else {
+      hoverX = mouse.x;
+      hoverY = mouse.y;
+    }
+
+    //Draw hovered location indicator
+    for(var i = 0; i <= settings.textureTilingX; i++) {
+      for(var j = 0; j <= settings.textureTilingY; j++) {
+        context.drawImage(hover, 0, 0, 50, 50, hoverX + 50 * i, hoverY + 50 * j, 50, 50);
+      }
+    }
+  }
 
   //Draw mouse position
   context.font = "16px Arial";
@@ -148,6 +220,7 @@ function updateTopLeft() {
   if(movement.right == true) {topleft.x += 10;}
   if(movement.up == true) {topleft.y -= 10;}
   if(movement.down == true) {topleft.y += 10;}
+  mouseMapPosition.recalculate();
 }
 
 //
@@ -155,8 +228,25 @@ function updateTopLeft() {
 //
 
 function handleCanvasClick(x, y) {
-  //x and y are the position of the click on the map
+  var trueX = (settings.gridlock == false) ? x : round(x, 50);
+  var trueY = (settings.gridlock == false) ? y : round(y, 50);
 
+  //Get Layer
+  var layer = parseInt(document.getElementById("layerButton").value);
+  if(layer < 0)
+    layer = 0;
+
+  //Make sure that layer's array has been initialized
+  if(textureMap.tiles[layer] == undefined)
+    textureMap.tiles[layer] = [];
+
+  //Add the tile
+  var obj = new Tile(selected.x, selected.y, selected.width, selected.height,
+    trueY, trueX, trueY + settings.textureTilingY * selected.height, trueX + settings.textureTilingX * selected.width, 0);
+
+  console.log(obj);
+
+  textureMap.tiles[layer].push(obj);
 }
 
 //
@@ -166,17 +256,21 @@ function handleCanvasClick(x, y) {
 function newMap() {
   if(!confirm("Are you sure? Current map will be discarded.")) {
     textureMap.spritesheet = document.getElementById("spritesheet").value;
-    wallMap.bounds.x = document.getElementById("xbounds").value;
-    wallMap.bounds.y = document.getElementById("ybounds").value;
+    blockMap.bounds.x = document.getElementById("xbounds").value;
+    blockMap.bounds.y = document.getElementById("ybounds").value;
 
     spritesheet.src = "sprites/" + textureMap.spritesheet;
+
+    //Call this function in spriteSelector.js
+    setNewSpritesheet(spritesheet.src);
+
     clearMap();
   }
 }
 
 function clearMap() {
   textureMap.tiles = [];
-  wallMap.tiles = [];
+  blockMap.tiles = [];
 }
 
 function saveMapToFile() {
@@ -200,10 +294,10 @@ function decodeTextureMap() {
 
 }
 
-function encodeWallMap() {
+function encodeblockMap() {
 
 }
 
-function decodeWallMap() {
+function decodeblockMap() {
 
 }
---
+++ Level Editor/spriteSelector.js
@@ -0,0 +1,96 @@
+var canvasSpritesheet = document.getElementById("spritesheet");
+var contextSpritesheet = canvasSpritesheet.getContext("2d");
+var spriteSelectorImage = new Image();
+spriteSelectorImage.src = "sprites/spritesheet1.png";
+
+const selectionBoxWidth = 4;
+const roundTo = 10;
+
+var mouseIsDown = false;
+selected = {
+  x:0,
+  y:0,
+  width:50,
+  height:50
+}
+
+//Top corner of canvas, for purposes of accurate drawing
+var canvasTopCorner = {
+  x:canvasSpritesheet.getBoundingClientRect().left,
+  y:canvasSpritesheet.getBoundingClientRect().top
+}
+
+function setNewSpritesheet(filename) {
+  spriteSelectorImage.src = filename;
+}
+
+function drawSpriteSheetSelector() {
+  //Clear the canvas
+  contextSpritesheet.clearRect(0, 0, canvasSpritesheet.width, canvasSpritesheet.height);
+
+  //Draw the spritesheet onto it
+  contextSpritesheet.drawImage(spriteSelectorImage, 0, 0, 400, 300, 0, 0, 400, 300);
+
+  //Draw the selection square
+  contextSpritesheet.beginPath();
+
+  contextSpritesheet.rect(selected.x + selectionBoxWidth / 2, selected.y + selectionBoxWidth / 2,
+    selected.width - selectionBoxWidth / 2, selected.height - selectionBoxWidth / 2);
+    contextSpritesheet.lineWidth = "" + selectionBoxWidth;
+    contextSpritesheet.strokeStyle = "rgba(0, 255, 255, 0.7)";
+    contextSpritesheet.stroke();
+  }
+
+  var update = setInterval(function() {
+    drawSpriteSheetSelector();
+  }, 1000/30);
+
+  //Selecting which area of spritesheet to use
+  canvasSpritesheet.onmousedown = function(event) {
+    selected.x = round(event.x - canvasTopCorner.x, roundTo);
+    selected.y = round(event.y - canvasTopCorner.y, roundTo);
+
+    selected.width = 0;
+    selected.height = 0;
+    mouseIsDown = true;
+  }
+
+canvasSpritesheet.onmousemove = function(event) {
+  var truePos = {
+    x: round(event.x - canvasTopCorner.x, roundTo),
+    y: round(event.y - canvasTopCorner.y, roundTo)
+  }
+
+  if(!mouseIsDown)
+    return;
+
+  if(truePos.x > selected.x) {
+    selected.width = truePos.x - selected.x;
+  } else {
+    selected.width += selected.x - truePos.x;
+    selected.x = truePos.x;
+  }
+
+  if(truePos.y > selected.y) {
+    selected.height = truePos.y - selected.y;
+  } else {
+    selected.height += selected.y - truePos.y;
+    selected.y = truePos.y;
+  }
+
+}
+
+canvasSpritesheet.onmouseup = function(event) {
+  mouseIsDown = false;
+}
+
+  canvasSpritesheet.addEventListener('dblclick', function(event) {
+    selected.x = round((event.x - canvasTopCorner.x),50);
+    selected.y = round((event.y - canvasTopCorner.y),50);
+    selected.width = 50;
+    selected.height = 50;
+  });
+
+function round(number, to) {
+  return number -= number % to;
+}

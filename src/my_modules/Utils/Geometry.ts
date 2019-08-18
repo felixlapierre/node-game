fbe687cf5558b990a7e2537f27a58a6c4f8b045f
Felix <felix.lapierre1@gmail.com>
2019-08-17 20:58:52 -0400

    Made shapes compile and started writing tests

--- src/my_modules/Utils/Geometry.ts
+++ src/my_modules/Utils/Geometry.ts
@@ -7,27 +7,47 @@ export class Point implements Shape {
 
     Overlaps(shape: Shape) {
         if (shape instanceof Point) {
-            return PointsOverlap(this, shape);
+            return PointPointOverlap(this, shape);
         }
         if (shape instanceof Rectangle) {
-            return PointAndRectangleOverlap(this, shape);
+            return PointRectangleOverlap(this, shape);
         }
         if (shape instanceof Circle) {
-            return PointAndCircleOverlap(this, shape);
+            return PointCircleOverlap(this, shape);
         }
+        throw new NoOverlapFunctionFoundError(this, shape);
     }
+}
+
+function PointPointOverlap(p1: Point, p2: Point) {
+    return p1.x == p2.x && p1.y == p2.y;
+}
+
+function PointCircleOverlap(point: Point, circle: Circle) {
+    return Distance(point, circle.center) < circle.radius;
+}
+
+function PointRectangleOverlap(point: Point, rectangle: Rectangle) {
+    return point.x < rectangle.Right()
+        && point.x > rectangle.Left()
+        && point.y < rectangle.Top()
+        && point.y > rectangle.Bottom();
 }
 
 export class Rectangle implements Shape {
     constructor(public corner: Point, public width: number, public height: number) { }
 
     Overlaps(shape: Shape) {
+        if (shape instanceof Point) {
+            return PointRectangleOverlap(shape, this);
+        }
         if (shape instanceof Rectangle) {
-            return RectanglesOverlap(this, shape);
+            return RectangleRectangleOverlap(this, shape);
         }
         if (shape instanceof Circle) {
-            return CircleAndRectangleOverlap(shape, this)
+            return CircleRectangleOverlap(shape, this)
         }
+        throw new NoOverlapFunctionFoundError(this, shape);
     }
 
     Left() { return this.corner.x }
@@ -47,52 +67,44 @@ export class Rectangle implements Shape {
     BottomRight() { return new Point(this.Right(), this.Bottom()) }
 }
 
+function RectangleRectangleOverlap(r1: Rectangle, r2: Rectangle) {
+    // If any corner of a rectangle is inside the other rectangle, they are overlapping.
+    return PointRectangleOverlap(r1.TopLeft(), r2)
+    || PointRectangleOverlap(r1.TopRight(), r2)
+    || PointRectangleOverlap(r1.BottomLeft(), r2)
+    || PointRectangleOverlap(r1.BottomRight(), r2);
+}
+
 export class Circle implements Shape {
     constructor(public center: Point, public radius: number) { }
 
     Overlaps(shape: Shape) {
         if (shape instanceof Rectangle) {
-            return CircleAndRectangleOverlap(this, shape);
+            return CircleRectangleOverlap(this, shape);
         }
         if (shape instanceof Circle) {
-            return CirclesOverlap(this, shape);
+            return CircleCircleOverlap(this, shape);
         }
+        throw new NoOverlapFunctionFoundError(this, shape);
     }
 }
 
-export function RectanglesOverlap(r1: Rectangle, r2: Rectangle) {
-    // If any corner of a rectangle is inside the other rectangle, they are overlapping.
-    return PointAndRectangleOverlap(r1.TopLeft(), r2)
-    || PointAndRectangleOverlap(r1.TopRight(), r2)
-    || PointAndRectangleOverlap(r1.BottomLeft(), r2)
-    || PointAndRectangleOverlap(r1.BottomRight(), r2);
-}
-
-export function CirclesOverlap(c1: Circle, c2: Circle) {
+function CircleCircleOverlap(c1: Circle, c2: Circle) {
     return Distance(c1.center, c2.center) < c1.radius + c2.radius
 }
 
-export function PointsOverlap(p1: Point, p2: Point) {
-    return p1.x == p2.x && p1.y == p2.y;
-}
-
-export function CircleAndRectangleOverlap(circle: Circle, rectangle: Rectangle) {
-
-}
-
-export function PointAndCircleOverlap(point: Point, circle: Circle) {
-    return Distance(point, circle.center) < circle.radius;
-}
-
-export function PointAndRectangleOverlap(point: Point, rectangle: Rectangle) {
-    return point.x < rectangle.Right()
-        && point.x > rectangle.Left()
-        && point.y < rectangle.Top()
-        && point.y > rectangle.Bottom();
+function CircleRectangleOverlap(circle: Circle, rectangle: Rectangle) {
+    return false;
 }
 
 export function Distance(point1: Point, point2: Point) {
     const deltaX = point2.x - point1.x;
     const deltaY = point2.y - point1.y;
     return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+}
+
+class NoOverlapFunctionFoundError extends Error {
+    constructor(shape1: Shape, shape2: Shape) {
+        super(`No overlap function found between shapes ${shape1.constructor.name} and ${shape2.constructor.name}`);
+    }
 }
--- test/Geometry.test.ts
+++ test/Geometry.test.ts
@@ -1,7 +1,20 @@
 import { expect } from 'chai';
+import { Point } from '../src/my_modules/Utils/Geometry';
 
-describe('Basic test', () => {
-    it('should pass', () => {
-        expect(true).to.be.true;
+describe('Point', () => {
+    describe('Overlaps', () => {
+        it('should be true for identical points', () => {
+            const p1 = new Point(5, 5);
+            const p2 = new Point(5, 5);
+
+            expect(p1.Overlaps(p2)).to.be.true;
+        })
+
+        it('should be false for non-identical points', () => {
+            const p1 = new Point(5, 5);
+            const p2 = new Point(5, 6);
+
+            expect(p1.Overlaps(p2)).to.be.false;
+        })
     })
 })

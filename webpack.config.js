f003e0ccb63994a939c6c32ad8e232322efbd2e1
Felix <felix.lapierre1@gmail.com>
2019-08-18 22:52:02 -0400

    Added webpack for compiling frontend

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/static/game.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: 'game.js',
+        path: path.resolve(__dirname, 'build/static')
+    }
+}
